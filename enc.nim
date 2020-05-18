baefb6bacfbf529fa9ddc5ec2e3010980687a04b
Panayotis Katsaloulis <panayotis@panayotis.com>
2020-05-18 18:36:43 +0300

    windows tweaks

--- enc.nim
+++ enc.nim
@@ -1,16 +1,25 @@
 import myexec, json, os
 
+const OPENSSL=
+    when system.hostOS == "windows": "openssl.exe"
+    else: "openssl"
+
+template echoNoLine(txt:string) =
+    stdout.write txt
+    stdout.flushFile
+
 proc storeKeystore*(content:JsonNode, location:string, pass:string) =
     location.parentDir.createDir
-    var(text,exitCode) = myExec(@["openssl", "enc", "-salt", "-aes-256-cbc", "-pass", "pass:"&pass, "-out", location], $content)
-    if exitCode != 0 or text.len>0:
+    var(text,exitCode) = myExec(@[OPENSSL, "enc", "-salt", "-a", "-aes-256-cbc", "-pass", "pass:"&pass, "-out", location], $content)
+    echoNoLine text
+    if exitCode != 0:
         quit("Unable to store keychain under " & location)
 
 proc readKeystore*(location:string, pass:string):JsonNode =
     var text = """{}"""
     if location.fileExists:
         var exitCode:int
-        (text,exitCode) = myExec(@["openssl", "enc", "-d", "-salt", "-aes-256-cbc", "-pass", "pass:"&pass, "-in", location])
-        if exitCode != 0 or text.len < 1: quit("Unable to read keystore")
+        (text,exitCode) = myExec(@[OPENSSL, "enc", "-d", "-salt", "-a", "-aes-256-cbc", "-pass", "pass:"&pass, "-in", location])
+        if exitCode != 0: quit("Unable to read keystore")
     result = text.parseJson
     result.storeKeystore(location, pass)
